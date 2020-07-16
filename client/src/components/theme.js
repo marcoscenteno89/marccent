d134fecce0a365eb9d5209f8b8c416a1c2a73be5
Marcos <marcoscenteno89@gmail.com>
2020-07-15 23:25:19 -0600

    added hover effect to theme switcher

--- client/src/components/theme.js
+++ client/src/components/theme.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import styled, { css, keyframes } from 'styled-components';
 const url = "http://localhost:5000/api/theme";
 
 class Theme extends Component {
@@ -8,7 +7,6 @@ class Theme extends Component {
         super(props);
         this.state = {
             themes: false,
-            active: false
         }
     }
 
@@ -18,24 +16,33 @@ class Theme extends Component {
             const jsonData = await res.json();
             this.setState({
                 themes: jsonData,
-                active: jsonData.filter(single => single.active > 0)[0]
             });
-            if (this.state.active !== false) this.renderActive(this.state.active);
+            this.renderCss(this.state.themes);
         } catch (err) {
             console.log(err.message);
         }
-        
     }
 
-    renderActive(active) {
-        const body = document.body;
-        const a = active.primary_color;
-        body.style.background = a;
-        body.style.color = a;
+    renderCss(themes) {
+        let link  = document.createElement('style');
+        link.type = 'text/css';
+        let text = '';
+        themes.map(a => (
+            text += `
+            nav {}
+            .single-${a.name}:hover {
+                animation: glow-${a.name} 1s ease-in-out infinite alternate;
+            }
+            @keyframes glow-${a.name} {
+                from {box-shadow: 0 0 10px #fff, 0 0 30px #fff, 0 0 50px ${a.primary_color}, 0 0 60px ${a.primary_color};}
+                to { box-shadow: 0 0 20px #fff, 0 0 40px #fff, 0 0 60px ${a.primary_color}, 0 0 70px ${a.primary_color};}
+            }`
+        ))
+        link.innerHTML = `<style>${text}</style>`;
+        document.getElementsByTagName('head')[0].appendChild(link);
     }
 
     render() {
-
         if (this.state.themes === false) return <footer className="theme">No theme found</footer>
         
         return (
@@ -55,19 +62,6 @@ class Choices extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            id: 0,
-            name: '',
-            primary_color: '',
-            secundary_color: '',
-            is_dark: 0,
-            active: 0,
-            expired: 0,
-            expiration_date_time: null
-        }
-    }
-
-    componentDidMount() {
-        this.setState({
             id: this.props.data.id,
             name: this.props.data.name,
             primary_color: this.props.data.primary_color,
@@ -76,29 +70,56 @@ class Choices extends Component {
             active: this.props.data.active,
             expired: this.props.data.expired,
             expiration_date_time: this.props.data.expiration_date_time
-        }); 
+        }
+    }
+
+    async componentDidMount() {
+        try {
+            this.setState({
+                id: this.props.data.id,
+                name: this.props.data.name,
+                primary_color: this.props.data.primary_color,
+                secundary_color: this.props.data.secundary_color,
+                is_dark: this.props.data.is_dark,
+                active: this.props.data.active,
+                expired: this.props.data.expired,
+                expiration_date_time: this.props.data.expiration_date_time
+            });
+            if (this.state.active === 1) this.renderActive(this.state);
+        } catch (err) {
+            console.log(err.message);
+        }
+        
+    }
+
+    renderActive(active) {
+        let link  = document.createElement('style');
+        link.type = 'text/css';
+        let text  = `
+            nav {}
+            body {
+                background-color: ${active.primary_color} !important;
+                color: ${active.primary_color} !important;
+            }
+            .single-active {
+                animation: glow-${active.name} 1s ease-in-out infinite alternate;
+            }`
+        link.innerHTML = `<style>${text}</style>`;
+        document.getElementsByTagName('head')[0].appendChild(link);
+    }
+
+    update(id) {
+        console.log(id);
     }
 
     render() {
-
-        // let classes = `single${this.state.active === 1 ? ' active' : ''}`;
-        const a = this.state.primary_color;
-        // const glow = keyframes`
-        //     0% { box-shadow: inset 0 0 60px #fff, inset 0 0 90px #fff, 0 0 10px #fff, 0 0 30px #fff, 0 0 50px ${a}, 0 0 60px ${a} }
-        //     100% { box-shadow: inset 0 0 90px #fff, inset 0 0 120px #fff, 0 0 20px #fff, 0 0 40px #fff, 0 0 60px ${a}, 0 0 70px ${a}; }
-        // `;
-        const styles = css`
-            animation: glow 1s ease-in-out infinite alternate;
-            @keyframes glow {
-                0% { box-shadow: inset 0 0 60px #fff, inset 0 0 90px #fff, 0 0 10px #fff, 0 0 30px #fff, 0 0 50px ${a}, 0 0 60px ${a} }
-                100% { box-shadow: inset 0 0 90px #fff, inset 0 0 120px #fff, 0 0 20px #fff, 0 0 40px #fff, 0 0 60px ${a}, 0 0 70px ${a}; }
-            }`
+        let classes = `single single-${this.state.name} ${this.state.active === 1 ? 'single-active' : ''}`;
 
         return (
-            <button key={this.state.id} className="single">
-                <i className="fas fa-fan small" style={{color: a}}></i>
-                <i className="fas fa-globe median" style={{color: a}}></i>
-                <i className="fab fa-hornbill large" style={{color: a}}></i>
+            <button key={this.state.id} className={classes} onClick={() => this.update(this.state.id)}>
+                <i className="fas fa-fan small" style={{color: this.state.primary_color}}></i>
+                <i className="fas fa-globe median" style={{color: this.state.primary_color}}></i>
+                <i className="fab fa-hornbill large" style={{color: this.state.primary_color}}></i>
             </button>
         )
     }
