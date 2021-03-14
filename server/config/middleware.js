6acc3d13d61e6e9152ce60829218d4c2cb164af6
marcos <marcoscenteno89@gmail.com>
2021-03-14 16:22:36 -0600

    random commit

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,7 +5,9 @@ module.exports = {
         origin: [
             'http://localhost:1337', 
             'http://localhost:3000', 
-            'marccent.com', 
+            'http://marccent.com',
+            'https://www.marccent.com',
+            'https://marccent.com'
         ],
         headers: ["Content-Type", "Authorization", "X-Frame-Options", "sentry-trace"]
       },
