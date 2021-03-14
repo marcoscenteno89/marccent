4c859cdc693f0ddff041840ccc8d30d2f548da55
marcos <marcoscenteno89@gmail.com>
2021-03-14 15:44:29 -0600

    updated cors

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -5,8 +5,7 @@ module.exports = {
         origin: [
             'http://localhost:1337', 
             'http://localhost:3000', 
-            'https://marccent.com', 
-            'https://www.marccent.com'
+            'marccent.com', 
         ],
         headers: ["Content-Type", "Authorization", "X-Frame-Options", "sentry-trace"]
       },
